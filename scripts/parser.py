e9432ebe78d409ab7bede8571437778735f2e26e
Ваше Имя <you@example.com>
2023-06-18 07:01:51 +0300

    fix

--- scripts/parser.py
+++ scripts/parser.py
@@ -2,38 +2,33 @@ from functools import reduce
 from subprocess import PIPE, Popen
 
 
+def collect_system_info(cmd):
+    info = Popen([cmd], shell=True, stdout=PIPE)
+    return list(info.stdout.read().decode('ascii').split())[1:]
+
+
+def get_max(process_list, data_list):
+    list_zipped = list(zip(process_list, data_list))
+    list_zipped.sort(key=lambda x: x[1], reverse=True)
+    return list_zipped[0][0]
+
+
 def parser():
+    user_list = set(collect_system_info('ps aux | tr -s " " | cut -d " " -f1'))
+    pid_list = set(collect_system_info('ps aux | tr -s " " | cut -d " " -f2'))
+    cpu_list = list(collect_system_info('ps aux | tr -s " " | cut -d " " -f3'))
+    mem_list = list(collect_system_info('ps aux | tr -s " " | cut -d " " -f4'))
+    process_list = list(collect_system_info('ps aux | tr -s " " | cut -d " " -f11'))
+
+    cpu_list2 = [float(x) for x in cpu_list[1:]]
+    sum_cpu = reduce((lambda x, y: x + y), cpu_list2)
+    mem_list2 = [float(x) for x in mem_list[1:]]
+    mem_sum = reduce((lambda x, y: x + y), mem_list2)
+
+    process_mem = get_max(process_list, mem_list)
+    process_cpu = get_max(process_list, cpu_list)
+
     with open('parsed_data.txt', 'w') as f:
-        user_list = Popen(
-            ['ps aux | tr -s " " | cut -d " " -f1'], shell=True, stdout=PIPE)
-        user_list = set(user_list.stdout.read().decode('ascii').split())
-
-        pid_list = Popen(
-            ['ps aux | tr -s " " | cut -d " " -f2'], shell=True, stdout=PIPE)
-        pid_list = set(pid_list.stdout.read().decode('ascii').split())
-
-        cpu_list = Popen(
-            ['ps aux | tr -s " " | cut -d " " -f3'], shell=True, stdout=PIPE)
-        cpu_list = list(cpu_list.stdout.read().decode('ascii').split())
-        cpu_list2 = [float(x) for x in cpu_list[1:]]
-        sum_cpu = reduce((lambda x, y: x + y), cpu_list2)
-
-        mem_list = Popen(
-            ['ps aux | tr -s " " | cut -d " " -f4'], shell=True, stdout=PIPE)
-        mem_list = list(mem_list.stdout.read().decode('ascii').split())
-        mem_list2 = [float(x) for x in mem_list[1:]]
-        mem_sum = reduce((lambda x, y: x + y), mem_list2)
-
-        process_mem_list = Popen(
-            ['ps aux --sort=-%mem | tr -s " " | cut -d " " -f11'], shell=True, stdout=PIPE)
-        process_mem_list = list(process_mem_list.stdout.read().decode('ascii').split())[1:]
-        process_mem = process_mem_list[0]
-
-        process_cpu_list = Popen(
-            ['ps aux --sort=-%cpu | tr -s " " | cut -d " " -f11'], shell=True, stdout=PIPE)
-        process_cpu_list = list(process_cpu_list.stdout.read().decode('ascii').split())[1:]
-        process_cpu = process_cpu_list[0]
-
         f.write("Отчёт о состоянии системы: \n")
         f.write(f'Пользователи системы: {", ".join(user_list)}\n')
         f.write(f'Процессов запущено: : {len(pid_list)}\n')
